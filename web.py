275550713bc5fc16007bdacf58c3c5f8f9b4c1a2
ali ibrahimli <alibrahimli041@gmail.com>
2023-05-11 00:43:35 +0400

    Some Errors resolved

--- web.py
+++ web.py
@@ -8,13 +8,19 @@ def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
+    st.session_state["new_todo"]=""
 
 
 todos = functions.get_todos()
 
-st.title("My Todo App")
-st.subheader("This is my todo app")
-st.write("This app is to increase your productivity")
+st.title("Planlama aplikasiyası")
+st.subheader("Bu aplikasiyadan istifadə edərək gündəlik işlərinizi daha planlı idarə edə bilərsiniz.")
+st.write("Tekst boşluğuna yeni planlarınızı yazın və Enter knopkasına klikləyin.\n"
+         "Hərhansısa bir işi bitirdikdə isə planın qarşısında olan checkbox-a klikləyin həmin plan listdən silinəcək.")
+
+
+st.text_input(label="Plan əlavə edin", placeholder="yeni planınızı əlavə edin...",
+              on_change=add_todo, key='new_todo', value=st.session_state.get('new_todo', ''))
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -24,5 +30,4 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="Enter a todo", placeholder="Add a new todo...",
-              on_change=add_todo, key='new_todo')
+
